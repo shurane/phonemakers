802a7e880aa432f29458e44b8ce212778bbf5639
Ehtesh Choudhury <merlockmagus@gmail.com>
2013-11-07 09:08:10 -0500

    WIP. What are the haps?

--- application.js
+++ application.js
@@ -1,10 +1,11 @@
 (function(window, undefined){
-    var document = window.document;
+    //var document = window.document;
     //dundun = {};  // XXX remove later
     $.getJSON("mdict.json", function(data){
         //var options = { item: 'hacker-item' };
         //var hackerList = new List('hacker-list', options, data["ZTE"]);
-        //dundun = data;
+        dundun = data;
+        phoneystump = dundun["Acer"][0];
 
         var listum = document.getElementById("hacker-list");
 
@@ -18,14 +19,21 @@
                 var element = document.createElement("div");
                 var para = document.createElement("p");
                 var anchor = document.createElement("a");
-                anchor.innerHHTML = phone.url;
+                var dimensions = document.createElement("p");
+
+                dimensions.innerHTML = phone.fields.Body.Dimensions;
+                anchor.innerHTML = phone.url;
                 para.innerHTML = phone.canonical_name;
+
                 element.appendChild(para);
                 element.appendChild(anchor);
+                element.appendChild(dimensions);
+
                 listum.appendChild(element);
                 count +=1;
                 if ( count % 50 === 0 ){
-                    console.log(count, phone);
+                    //console.log(count, phone);
+                    console.log(element)
                 }
             }
         }
