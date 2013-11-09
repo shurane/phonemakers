8273d73890dff6c2e5ca5f7252133d7fb5477036
Ehtesh Choudhury <merlockmagus@gmail.com>
2013-11-09 12:24:30 -0500

    iterating through values successfully. Initial attempt with handlebars.js

--- application.js
+++ application.js
@@ -1,43 +1,41 @@
 (function(window, undefined){
     //var document = window.document;
     //dundun = {};  // XXX remove later
+    source = $("#entry-template").html();
+    template = Handlebars.compile(source);
     $.getJSON("mdict.json", function(data){
         //var options = { item: 'hacker-item' };
         //var hackerList = new List('hacker-list', options, data["ZTE"]);
-        dundun = data;
+        dundun = data; // XXX remove later
         phoneystump = dundun["Acer"][0];
+        console.log(template(phoneystump));
 
-        var listum = document.getElementById("hacker-list");
+        //var listum = document.getElementById("hacker-list");
 
-        var count = 0;
-        for(var makerName in data){
-            console.log(makerName);
-            var maker = data[makerName];
-            for (var phoneName in maker){
-                var phone = maker[phoneName];
+        //var count = 0;
+        //for(var makerName in data){
+            //var maker = data[makerName];
+            //for (var phoneName in maker){
+                //count +=1;
+                //if ( count % 1200 === 0 ){
+                    //var phone = maker[phoneName];
+                    //var element = document.createElement("div");
 
-                var element = document.createElement("div");
-                var para = document.createElement("p");
-                var anchor = document.createElement("a");
-                var dimensions = document.createElement("p");
-
-                dimensions.innerHTML = phone.fields.Body.Dimensions;
-                anchor.innerHTML = phone.url;
-                para.innerHTML = phone.canonical_name;
-
-                element.appendChild(para);
-                element.appendChild(anchor);
-                element.appendChild(dimensions);
-
-                listum.appendChild(element);
-                count +=1;
-                if ( count % 50 === 0 ){
-                    //console.log(count, phone);
-                    console.log(element)
-                }
-            }
-        }
-        console.log(count);
+                    //console.log("========================================");
+                    //var okayFields = [ "Dimensions", "Body", "OS", "CPU", "SIZE"];
+                    //for (var fieldName in phone.fields){
+                        //var fieldElem = document.createElement("p");
+                        //console.log("====");
+                        //console.log("Field::"+ fieldName);
+                        //for (var sectionName in phone.fields[fieldName]){
+                            //var sectionElem = document.createElement("p");
+                            //console.log("Section::" + sectionName + "::" + phone.fields[fieldName][sectionName]);
+                        //}
+                    //}
+                //}
+            //}
+        //}
+        //console.log(count);
 
     });
 }());
