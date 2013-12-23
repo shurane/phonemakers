865a27bf20c219259fc0385e2a8b58fe93efac37
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2013-12-23 17:44:02 -0500

    temporary changes with Will's offers

--- application.js
+++ application.js
@@ -28,6 +28,30 @@
         comparator: 'name'
     });
 
+    // // William Saunders mentions typing a View to typeahead
+    //var SearchView = Backbone.View.extend({
+        //render: function() {
+            ////this.$el.clear();
+            //this.$el.html( this.searchTemplate( someContext ) );
+            //this.$('.example-phones .typeahead').typeahead({                                
+                //name: 'countries',                                                          
+                //prefetch: '../data/countries.json',                                         
+                //limit: 10                                                                   
+            //});
+            //return this;
+        //},
+        //searchTemplate: _.template( $( "script#typeAheadBox" ) )
+    //});
+
+    var PhoneView = Backbone.View.extend({
+        tagName : "div",
+        className : "phone",
+        render : function (){
+            // use mustache templates here
+            this.el.innerHTML = this.model.get('canonical_name'); 
+        }
+    });
+
     nexusone = new Phone({ name:"Google Nexus One", maker: "Google"});
     Phones = new PhoneList();
     Phones.fetch();
